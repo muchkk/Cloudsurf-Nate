a388772faf5ea5e7b6ab4ac87b4f74c5b276d894
muchkk <nathaniel.vonhartleben@gmail.com>
2024-05-07 17:44:21 -0700

    info update

--- React-Project/react-website/src/components/Home.js
+++ React-Project/react-website/src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Nav from "./Nav";
+import img_1412 from "../IMG_1412.jpg"
+import Contacts from './Contacts';
 
 // Import your other components
 // import About from './About';
@@ -10,6 +12,7 @@ import Nav from "./Nav";
 const styles = {
   homeContainer: {
     fontFamily: 'Poppins, sans-serif',
+    maxWidth: '100%',
     // backgroundColor: '#3a1a1f',
   },
 };
@@ -19,7 +22,12 @@ function Home() {
       <div style={styles.homeContainer}>
         <Nav />
         <div>
-          <h1>BRUH</h1>
+          <h1>Software Developer</h1>
+          <h2>To continuously enhance skills, design innovative and scalable software solutions that drive business growth, improve user experiences, and meet diverse client needs through cutting-edge technologies.</h2>
+          <img src={img_1412} alt="Profile photo" style={{width: "200px"}} />
+          <h3>Resume: UAT Analyst - Nordstrom Credit, CDP Analyst - Nordstrom Credit</h3>
+          <h4>This website</h4>
+          <Contacts />
         </div>
       </div>
   );
