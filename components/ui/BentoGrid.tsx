e101c274c7f6921fe25b6be61148fdc9d9eb278c
muchkk <nathaniel.vonhartleben@gmail.com>
2024-06-27 20:35:29 -0700

    fixed text animation and copy button

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -60,7 +60,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
+        "row-span-1 z-50 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       style={{
@@ -80,7 +80,7 @@ export const BentoGridItem = ({
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-100 ${
+          className={`absolute right-0 -bottom-5 ${
             id === 5 && "w-full opacity-80"
           }`}
         >
@@ -100,7 +100,7 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            "group-hover/bento:translate-x-2 transition-200 duration-500 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
