04f877a846608329f2417919a44d562d1fa69b0a
muchkk <nathaniel.vonhartleben@gmail.com>
2024-06-27 18:33:56 -0700

    adjusted photo for mobile and changed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -15,6 +16,7 @@ export default function Home() {
         />
         <Hero />
         <Grid />
+        <Footer />
       </div>
     </main>
   );
